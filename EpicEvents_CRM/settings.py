46451b62fc583f9d10cba7807f41440e3d08a299
franfif <francis.barrow@gmail.com>
2023-09-14 16:00:06 -0400

    Add contracts app and models

--- EpicEvents_CRM/settings.py
+++ EpicEvents_CRM/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "authentication",
     "clients",
+    "contracts",
 ]
 
 MIDDLEWARE = [
---
+++ contracts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from clients.models import Client
+
+
+class ContractStatus(models.Model):
+    STATUS_CHOICES = [
+        ("SIG", "Signed"),
+        ("UNS", "Not Signed"),
+        ("PYD", "Payed"),
+    ]
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3, unique=True)
+
+
+class Contract(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    status = models.ForeignKey(ContractStatus, on_delete=models.PROTECT)
+    amount = models.DecimalField(decimal_places=2, max_digits=16)
+    Payment_due = models.DateTimeField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    date_updated = models.DateTimeField(null=True)
