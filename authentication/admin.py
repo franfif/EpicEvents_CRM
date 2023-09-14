5c621f05a3ae4fce633ae223ce33d7025b45659f
franfif <francis.barrow@gmail.com>
2023-09-14 14:28:59 -0400

    Add UserRole to authentication app

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from authentication.models import User
+from authentication.models import User, UserRole
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(UserRole)
--- authentication/models.py
+++ authentication/models.py
@@ -2,8 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class UserRole(models.Model):
+    ROLE_CHOICES = [
+        ("SAL", "Sales Team"),
+        ("SUP", "Support Team"),
+        ("MAN", "Management"),
+    ]
+    role = models.CharField(choices=ROLE_CHOICES, max_length=3, unique=True)
+
+
 class User(AbstractUser):
-    pass
+    role = models.ForeignKey(UserRole, on_delete=models.PROTECT)
     first_name = models.CharField(max_length=25, blank=True)
     last_name = models.CharField(max_length=25, blank=True)
     email = models.EmailField(blank=True)
