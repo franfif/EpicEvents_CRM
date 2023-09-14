a812ec72ac0d43b17b93d205c2d2385d1e480ee3
franfif <francis.barrow@gmail.com>
2023-09-14 14:28:41 -0400

    Complete User model

--- authentication/models.py
+++ authentication/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+    first_name = models.CharField(max_length=25, blank=True)
+    last_name = models.CharField(max_length=25, blank=True)
+    email = models.EmailField(blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    mobile_number = models.CharField(max_length=20, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    date_updated = models.DateTimeField(null=True)
