2a3cf380919bfb34efca3dfbd14db228717381d0
franfif <francis.barrow@gmail.com>
2023-09-19 16:53:50 -0400

    add Client List Create view

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from clients import serializers, models
+from clients.permissions import IsSalesTeamMemberOrReadOnly
+
+from authentication.models import UserRole
+
+
+class ClientListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsSalesTeamMemberOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return serializers.ClientCreateSerializer
+        return serializers.ClientListSerializer
+
+    def get_queryset(self):
+        if self.request.user.role == UserRole.objects.get(role=UserRole.SALES_TEAM):
+            return models.Client.objects.all()
+        if self.request.user.role == UserRole.objects.get(role=UserRole.SUPPORT_TEAM):
+            return models.Client.objects.filter(
+                contracts__event__support_contact=self.request.user
+            )
+
+    def perform_create(self, serializer):
+        status = models.ClientStatus.objects.get(status="PRO")
+        client = serializer.save(sales_contact=self.request.user)
+        client.status = status
+        client.save()
+
+    # "status",
+    # "sales_contact",
