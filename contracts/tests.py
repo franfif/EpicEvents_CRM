51e4553e1fe3677d8a881720607916f835118a6e
franfif <francis.barrow@gmail.com>
2023-09-22 14:30:18 -0400

    add test for creating a contract

--- contracts/tests.py
+++ contracts/tests.py
@@ -61,3 +61,53 @@ class TestContract(ContractAPITestCase):
 
                 self.assertEqual(response.status_code, expected_status_code)
                 self.assertEqual(response.json(), expected_json)
+
+    def test_contract_create(self):
+        test_contract_create_params = [
+            # Unauthenticated user
+            (None, 401, {"detail": "Authentication credentials were not provided."}),
+            # Unauthorized user with wrong role
+            (
+                self.test_support_team_member,
+                403,
+                {"detail": "You do not have permission to perform this action."},
+            ),
+            # Authorized user (correct role)
+            (
+                self.test_sales_team_member,
+                201,
+                {
+                    "client": self.test_client_1.pk,
+                    "status": self.test_status_signed.pk,
+                    "amount": "25000.00",
+                    "payment_due": "2023-09-07T00:00:00Z",
+                },
+            ),
+        ]
+        for (
+            test_user,
+            expected_status_code,
+            expected_json,
+        ) in test_contract_create_params:
+            with self.subTest(
+                test_user=test_user,
+                expected_status_code=expected_status_code,
+                expected_json=expected_json,
+            ):
+                self.client.force_authenticate(user=test_user)
+                response = self.client.post(
+                    self.url_contract_list,
+                    data={
+                        "client": self.test_client_1.pk,
+                        "status": self.test_status_signed.pk,
+                        "amount": 25000.00,
+                        "payment_due": "2023-09-07T00:00:00Z",
+                    },
+                )
+                try:
+                    expected_json["id"] = response.json()["id"]
+                except KeyError:
+                    pass
+
+                # self.assertEqual(response.status_code, expected_status_code)
+                self.assertEqual(response.json(), expected_json)
