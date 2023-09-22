02fe31d98adfef27e78013824be1061d51d33184
franfif <francis.barrow@gmail.com>
2023-09-22 09:10:35 -0400

    initial test setup for contract

--- contracts/tests.py
+++ contracts/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.urls import reverse_lazy
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from unittest import mock
+
+from contracts.models import Contract, ContractStatus
+from authentication.models import User
+
+
+class ContractAPISetup(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_sales_team_member = User.objects.create_user()
+
+
+# class TestContract(ContractAPISetup):
+#     def test_list(self):
+#         self.client.force_authenticate(user=)
