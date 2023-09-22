9e34e97e2e6cf02508f837abbc606c08a6804b6d
franfif <francis.barrow@gmail.com>
2023-09-22 13:24:55 -0400

    add list view for contract

--- contracts/views.py
+++ contracts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from contracts import serializers, models
+from authentication.models import UserRole
+
+# from contract.permissions import
+
+from datetime import datetime
+
+
+class ContractQuerysetMixin:
+    def get_queryset(self):
+        if self.request.user.role == UserRole.objects.get(role=UserRole.SALES_TEAM):
+            return models.Contract.objects.all()
+        if self.request.user.role == UserRole.objects.get(role=UserRole.SUPPORT_TEAM):
+            return models.Contract.objects.filter(
+                event__support_contact=self.request.user
+            )
+
+
+class ContractListCreateAPIView(ContractQuerysetMixin, generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return serializers.ContractCreateSerializer
+        return serializers.ContractListSerializer
+
+
+class ContractDetailAPIView(ContractQuerysetMixin, generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = serializers.ContractDetailSerializer
+    lookup_field = "id"
+
+    def perform_update(self, serializer):
+        serializer.save(date_updated=datetime.now())
