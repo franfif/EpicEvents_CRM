9121f6c2c6fe8fc7fc447fa71f0c9c926d947998
franfif <francis.barrow@gmail.com>
2023-09-22 09:27:01 -0400

    refactor setUpTestData to tests module for DRY code

--- clients/tests.py
+++ clients/tests.py
@@ -1,67 +1,10 @@
-from django.urls import reverse_lazy
-from rest_framework.test import APITestCase
-
 from unittest import mock
 
-from clients.models import Client, ClientStatus
+from tests.test_setup import ProjectAPITestCase
 from clients.mocks import mock_perform_update, TEST_UPDATE_TIME
-from authentication.models import UserRole, User
 
 
-class ClientAPITestCase(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        ClientStatus.objects.create(status=ClientStatus.PROSPECT)
-        cls.status_existing = ClientStatus.objects.create(status=ClientStatus.EXISTING)
-
-        test_sales_team_role = UserRole.objects.create(role=UserRole.SALES_TEAM)
-        test_support_team_role = UserRole.objects.create(role=UserRole.SUPPORT_TEAM)
-        cls.test_sales_team_member = User.objects.create_user(
-            username="sales_tester",
-            email="test_sales@epic.com",
-            role=test_sales_team_role,
-            password="s@l3s_73573r",
-        )
-        cls.test_sales_team_member_2 = User.objects.create_user(
-            username="sales_tester_2",
-            email="test2_sales@epic.com",
-            role=test_sales_team_role,
-            password="s@l3s_73573r",
-        )
-        cls.test_support_team_member = User.objects.create_user(
-            username="support_tester",
-            email="test_support@epic.com",
-            role=test_support_team_role,
-            password="su990r7_73573r",
-        )
-
-        cls.test_client_1 = Client.objects.create(
-            company_name="Apple",
-            sales_contact=cls.test_sales_team_member,
-            first_name="Tim",
-            last_name="Cook",
-            email="tim.cook@apple.com",
-            phone_number="5552342123",
-        )
-        cls.test_client_2 = Client.objects.create(
-            company_name="Microsoft",
-            sales_contact=cls.test_sales_team_member,
-            first_name="Bill",
-            last_name="Gates",
-            email="bill.gates@microsoft.com",
-            phone_number="5554567859",
-        )
-
-        cls.url_detail = reverse_lazy(
-            "client-detail", kwargs={"id": cls.test_client_1.id}
-        )
-
-        cls.url_list = reverse_lazy("client-list")
-
-    def format_datetime(self, value):
-        if value:
-            return value.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-
+class ClientAPITestCase(ProjectAPITestCase):
     def get_client_list_data(self, clients):
         return [
             {
