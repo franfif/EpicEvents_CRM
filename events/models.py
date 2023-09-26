53e5e4b3a20b9854a4b944fb22ca78f1d1c9ebb8
franfif <francis.barrow@gmail.com>
2023-09-26 11:46:06 -0400

    update event model

--- events/models.py
+++ events/models.py
@@ -6,10 +6,14 @@ from contracts.models import Contract
 
 
 class EventStatus(models.Model):
+    CREATED = "CRE"
+    IN_PROCESS = "PRO"
+    ENDED = "END"
+
     STATUS_CHOICES = [
-        ("CRE", "Created"),
-        ("PRO", "In Process"),
-        ("END", "Ended"),
+        (CREATED, "Created"),
+        (IN_PROCESS, "In Process"),
+        (ENDED, "Ended"),
     ]
     status = models.CharField(choices=STATUS_CHOICES, max_length=3, unique=True)
 
@@ -18,7 +22,6 @@ class EventStatus(models.Model):
 
 
 class Event(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="events")
     contract = models.OneToOneField(
         Contract, on_delete=models.CASCADE, related_name="event"
     )
@@ -26,8 +29,8 @@ class Event(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="events"
     )
     status = models.ForeignKey(EventStatus, on_delete=models.PROTECT)
-    attendees = models.IntegerField()
+    attendees = models.IntegerField(null=True, blank=True)
     event_date = models.DateTimeField(null=True, blank=True)
-    notes = models.TextField()
+    notes = models.TextField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_updated = models.DateTimeField(null=True)
